20014cfa87098dee8ca84e403266559f2fde1eb0
Menna-Baligh <mennabaligh55520@yahoo.com>
2025-07-14 19:16:20 +0300

    Refactor routes to remove unused welcome view and organize cart-related routes

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,19 @@
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CartController;
 use App\Http\Controllers\FoodController;
 use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Auth::routes();
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::get('/foods/{id}',[FoodController::class, 'show'])->name('foods.show');
-Route::post('/foods/{id}',[FoodController::class, 'addToCart'])->name('cart.store');
 
+Route::get('/foods/{id}', [FoodController::class, 'show'])->name('foods.show');
+
+Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
+Route::post('/cart/{id}', [CartController::class, 'store'])->name('cart.store');
+Route::delete('/cart/{id}', [CartController::class, 'delete'])->name('cart.delete');
+Route::put('/cart/{id}', [CartController::class, 'update'])->name('cart.update');
