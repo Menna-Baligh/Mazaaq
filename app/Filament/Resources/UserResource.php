b92b7ef3fdfd03baeab323f827470700e91328cf
Menna-Baligh <mennabaligh55520@yahoo.com>
2025-09-03 18:29:35 +0300

    Enhance UserResource form and table with improved fields and actions

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -2,16 +2,27 @@
 
 namespace App\Filament\Resources;
 
-use App\Filament\Resources\UserResource\Pages;
-use App\Filament\Resources\UserResource\RelationManagers;
-use App\Models\User;
 use Filament\Forms;
-use Filament\Forms\Form;
-use Filament\Resources\Resource;
+use App\Models\User;
 use Filament\Tables;
+use Filament\Forms\Form;
 use Filament\Tables\Table;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\Group;
+use Filament\Forms\Components\Toggle;
+use Filament\Forms\Components\Section;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Actions\ViewAction;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use Filament\Tables\Actions\ActionGroup;
+use Filament\Tables\Actions\DeleteAction;
 use Illuminate\Database\Eloquent\Builder;
+use Filament\Tables\Filters\TernaryFilter;
+use App\Filament\Resources\UserResource\Pages;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\UserResource\RelationManagers;
 
 class UserResource extends Resource
 {
@@ -25,7 +36,49 @@ class UserResource extends Resource
     {
         return $form
             ->schema([
-                //
+                Group::make()
+                    ->schema([
+                        section::make()
+                            ->schema([
+                                TextInput::make('name')
+                                    ->required()
+                                    ->maxLength(50),
+
+                                TextInput::make('email')
+                                    ->email()
+                                    ->unique(ignoreRecord:true)
+                                    ->required()
+                                    ->maxLength(255),
+
+                                TextInput::make('password')
+                                    ->password()
+                                    ->confirmed()
+                                    ->required()
+                                    ->visible(fn (string $context): bool => $context === 'create')
+                                    ->maxLength(255),
+
+                                TextInput::make('password_confirmation')
+                                    ->password()
+                                    ->required()
+                                    ->visible(fn (string $context): bool => $context === 'create')
+                                    ->maxLength(255),
+
+                                TextInput::make('created_at')
+                                    ->label('Created At')
+                                    ->disabled()
+                                    ->columnSpanFull(),
+
+                                Toggle::make('is_admin')
+                                    ->label('Admin')
+                                    ->onIcon('heroicon-m-bolt')
+                                    ->offIcon('heroicon-m-user')
+                                    ->helperText('If checked, the user will have admin privileges.')
+                                    ->columnSpanFull()
+
+
+                            ])->columns(2),
+
+                    ])->columnSpanFull(),
             ]);
     }
 
@@ -33,13 +86,36 @@ class UserResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('name')
+                    ->sortable()
+                    ->searchable(),
+
+                TextColumn::make('email')
+                    ->sortable()
+                    ->searchable(),
+
+                TextColumn::make('updated_at')
+                    ->date(),
+
+                IconColumn::make('is_admin')
+                    ->boolean(),
+
+
             ])
             ->filters([
-                //
+                TernaryFilter::make('is_admin')
+                ->label('Type Of User')
+                ->boolean()
+                ->trueLabel('Admin')
+                ->falseLabel('User')
+                ->native(false)
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
+                ActionGroup::make([
+                    ViewAction::make(),
+                    EditAction::make(),
+                    DeleteAction::make(),
+                ])
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
